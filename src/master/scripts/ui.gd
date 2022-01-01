3c8251c597ea1c44619bd34c917509cd512f0b5d
CowKeyMan <dancauchi1@gmail.com>
2022-01-01 20:42:39 +0200

    Add first screen UI

---
+++ src/master/scripts/ui.gd
@@ -0,0 +1,15 @@
+extends Control
+
+signal text_changed(text)
+
+onready var text_edit = $VBoxContainer/TextEdit
+
+func _ready():
+	text_edit.connect("text_changed", self, "_on_TextEdit_text_changed")
+
+func _on_TextEdit_text_changed():
+	emit_signal("text_changed", text_edit.text)
+
+func get_text():
+	return text_edit.text
+
